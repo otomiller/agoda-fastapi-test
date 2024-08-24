4c72d1f566e2350721b325007bd0d059f060ba8f
Oto Miller <127068579+otomiller@users.noreply.github.com>
2024-08-24 19:31:43 -0400

    Update availability.py

--- api/app/services/availability.py
+++ api/app/services/availability.py
@@ -14,6 +14,13 @@ async def fetch_availability(room_request: RoomRequest):
             "cityId": room_request.cityId,
             "siteID": 1923846,
         }
-        response = await client.get(settings.AVAILABILITY_API_URL, params=params)
-        response.raise_for_status()  # This will raise an exception for HTTP errors
-        return response.json()
+        try:
+            response = await client.get(settings.AVAILABILITY_API_URL, params=params)
+            response.raise_for_status()  # This will raise an exception for HTTP errors
+            return response.json()
+        except httpx.HTTPError as e:
+            print(f"HTTP Exception for {e.request.url} - {e}")
+            return {"properties": []}  # Return an empty list of properties on error
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return {"properties": []}  # Return an empty list of properties on error
