b9042fbf14ef578e0ee00fe81de19ff0dbb8f9ba
Oto Miller <127068579+otomiller@users.noreply.github.com>
2024-08-24 19:25:41 -0400

    Update config.py

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -6,6 +6,12 @@ class Settings(BaseSettings):
     API_KEY: str = "1234567890abcdef"  # Replace with your actual API key
     AVAILABILITY_API_URL: str = "https://api.example.com/availability"  # Replace with the actual API URL
     DATABASE_URL: str = "postgresql://postgres:9vtTWvyxyHCpEvq@10.0.255.250:5432/agoda"
+    DB_NAME: str = "agoda"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "9vtTWvyxyHCpEvq"
+    DB_HOST: str = "10.0.255.250"
+    DB_PORT: str = "5432"
+    REDIS_URL: str = "redis://10.0.255.234:6379/0"
 
     class Config:
         env_file = ".env"
