093883987c32a299a8a5a359fbe34a811239229c
Oto Miller <127068579+otomiller@users.noreply.github.com>
2024-08-24 19:27:07 -0400

    Update database_config.py

--- api/app/core/database_config.py
+++ api/app/core/database_config.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from app.core.config import settings
 
-SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://")
+SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
 
 engine = create_async_engine(
     SQLALCHEMY_DATABASE_URL,
